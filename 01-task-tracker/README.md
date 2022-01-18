9d772e77330ebc6b829d63d5b9bab352185b98f5
solygambas <51904909+solygambas@users.noreply.github.com>
2022-01-18 08:37:51 +0100

    add header and button

--- 01-task-tracker/README.md
+++ 01-task-tracker/README.md
@@ -10,5 +10,7 @@ A simple to-do list app.
 
 - setting up an Angular app.
 - understanding modular structure, defining global styles and using interpolations.
+- generating components, handling input data and inline styles.
+- managing events with Output and EventEmitter.
 
 Based on [Angular Crash Course](https://www.youtube.com/watch?v=3dHNOWTI7H8) by Brad Traversy (2021).
--- 01-task-tracker/src/app/app.module.ts
+++ 01-task-tracker/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
