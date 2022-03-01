21cbb5d9dac4fd2ab5a41ce186ea8c8d4d6ff806
solygambas <51904909+solygambas@users.noreply.github.com>
2022-03-01 07:53:28 +0100

    add typeScript basics

---
+++ playground/01-finance-logger/README.md
@@ -0,0 +1,11 @@
+# Finance Logger
+
+A small project to learn TypeScript.
+
+## Features
+
+- compiling TypeScript into JavaScript.
+- understanding the basic types: strings, numbers and booleans.
+- working with objects and arrays.
+
+Based on [TypeScript Tutorial](https://www.youtube.com/playlist?list=PL4cUxeGkcC9gUgr39Q_yD6v-bSyMwKPUI) by Shaun Pelling - The Net Ninja (2020).
---
+++ playground/01-finance-logger/sandbox.js
@@ -0,0 +1,46 @@
+// npm i -g typescript
+// tsc sandbox.tsb -w
+// const character = "luigi";
+// console.log(character);
+// const inputs = document.querySelectorAll("input");
+// console.log(inputs);
+// inputs.forEach((input) => console.log(input));
+// TYPE BASICS
+// let character = "luigi";
+// let age = 30;
+// let isBlackBelt = false;
+// // character = 30;
+// character = "mario";
+// // age = 'yoshi'
+// age = 40;
+// // isBlackBelt = 'yes';
+// isBlackBelt = true;
+// const circ = (diameter: number) => diameter * Math.PI;
+// // console.log(circ("hello"));
+// console.log(circ(7.5));
+// OBJECTS & ARRAYS
+// arrays
+var names = ["luigi", "mario", "yoshi"];
+// names = 'hello'
+// names.push(3)
+// names[0] = 3;
+names.push("toad");
+var numbers = [10, 20, 30, 40];
+// numbers.push('luigi')
+// numbers[1] = "shaun"
+numbers.push(25);
+var mixed = ["ken", 5, "chun-li", 8, 9];
+mixed.push("ryu");
+mixed.push(10);
+mixed[0] = 3;
+// objects
+var ninja = { name: "mario", belt: "black", age: 30 };
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking'];
+ninja = {
+    name: "yoshi",
+    belt: "orange",
+    age: 40
+};
+ninja.name = "ryu";
+ninja.age = 30;
