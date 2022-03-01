2cfbd243b36539366abd2e7255ebfbd527aa5e32
solygambas <51904909+solygambas@users.noreply.github.com>
2022-03-01 08:24:34 +0100

    add explicit and dynamic types

--- playground/01-finance-logger/README.md
+++ playground/01-finance-logger/README.md
@@ -7,5 +7,7 @@ A small project to learn TypeScript.
 - compiling TypeScript into JavaScript.
 - understanding the basic types: strings, numbers and booleans.
 - working with objects and arrays.
+- defining explicit types.
+- creating dynamic types with 'any'.
 
 Based on [TypeScript Tutorial](https://www.youtube.com/playlist?list=PL4cUxeGkcC9gUgr39Q_yD6v-bSyMwKPUI) by Shaun Pelling - The Net Ninja (2020).
--- playground/01-finance-logger/sandbox.ts
+++ playground/01-finance-logger/sandbox.ts
@@ -32,30 +32,86 @@
 
 // OBJECTS & ARRAYS
 // arrays
-let names = ["luigi", "mario", "yoshi"];
-// names = 'hello'
-// names.push(3)
-// names[0] = 3;
-names.push("toad");
+// let names = ["luigi", "mario", "yoshi"];
+// // names = 'hello'
+// // names.push(3)
+// // names[0] = 3;
+// names.push("toad");
 
-let numbers = [10, 20, 30, 40];
-// numbers.push('luigi')
-// numbers[1] = "shaun"
-numbers.push(25);
+// let numbers = [10, 20, 30, 40];
+// // numbers.push('luigi')
+// // numbers[1] = "shaun"
+// numbers.push(25);
 
-let mixed = ["ken", 5, "chun-li", 8, 9];
-mixed.push("ryu");
-mixed.push(10);
-mixed[0] = 3;
+// let mixed = ["ken", 5, "chun-li", 8, 9];
+// mixed.push("ryu");
+// mixed.push(10);
+// mixed[0] = 3;
 
-// objects
-let ninja = { name: "mario", belt: "black", age: 30 };
-// ninja.age = '30';
-// ninja.skills = ['fighting', 'sneaking'];
-ninja = {
-  name: "yoshi",
-  belt: "orange",
-  age: 40,
-};
-ninja.name = "ryu";
-ninja.age = 30;
+// // objects
+// let ninja = { name: "mario", belt: "black", age: 30 };
+// // ninja.age = '30';
+// // ninja.skills = ['fighting', 'sneaking'];
+// ninja = {
+//   name: "yoshi",
+//   belt: "orange",
+//   age: 40,
+// };
+// ninja.name = "ryu";
+// ninja.age = 30;
+
+// EXPLICIT TYPES
+// let character: string;
+// let age: number;
+// let isLoggedIn: boolean;
+
+// // age = 'luigi';
+// age = 30;
+// // isLoggedIn = 25;
+// isLoggedIn = true;
+
+// // arrays
+// let ninjas: string[] = [];
+// ninjas.push("shaun");
+// ninjas = ["yoshi", "mario"];
+
+// // union types
+// let mixed: (string | number | boolean)[] = [];
+// mixed.push(false);
+// mixed.push(20);
+// mixed.push("hello");
+
+// let uid: string | number;
+// // uid = false
+// uid = "123";
+// uid = 123;
+
+// // objects
+// let ninjaOne: object;
+// ninjaOne = { name: "yoshi", age: 30 };
+// let ninjaTwo: {
+//   name: string;
+//   age: number;
+//   belt: string;
+// };
+// ninjaTwo = {
+//   name: "mario",
+//   belt: "black",
+//   age: 20,
+// };
+
+// DYNAMIC (ANY) TYPES
+// unkown and never are more appropriate
+// https://mariusschulz.com/blog/the-unknown-type-in-typescript
+// The unknown type is only assignable to the any type and the unknown type itself.
+// https://www.tutorialsteacher.com/typescript/typescript-never
+let age: any = 25;
+age = true;
+age = "hello";
+age = { name: "luigi" };
+let mixed: any[] = [];
+mixed.push(5);
+mixed.push("mario");
+mixed.push(false);
+let ninja: { name: any; age: any };
+ninja = { name: "yoshi", age: 25 };
