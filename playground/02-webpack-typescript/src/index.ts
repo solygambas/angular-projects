818890a09bfa392edc68319eccf7076d96fd228c
solygambas <51904909+solygambas@users.noreply.github.com>
2022-03-03 08:40:15 +0100

    add webpack

--- playground/02-webpack-typescript/src/index.ts
+++ playground/02-webpack-typescript/src/index.ts
@@ -1,1 +1,20 @@
-console.log("hello");
+import { formData } from "./forms";
+
+// console.log("hello");
+// console.log("hello again");
+// console.log("hello again again");
+
+const form = document.querySelector("form")!;
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const data = formData(form);
+  console.log(data);
+});
+
+// let person: any = [];
+// console.log(person.speak());
+
+// source maps
+// person.speak is not a function
+// at eval (index.ts?ffb4:16:20)
--- playground/02-webpack-typescript/webpack.config.js
+++ playground/02-webpack-typescript/webpack.config.js
@@ -1,18 +1,23 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development", // production
+  mode: "development",
+  devtool: "eval-source-map",
   entry: "./src/index.ts",
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: "ts-loader",
         include: [path.resolve(__dirname, "src")],
+        use: "ts-loader",
       },
     ],
   },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
   output: {
+    publicPath: "public",
     filename: "bundle.js",
     path: path.resolve(__dirname, "public"),
   },
