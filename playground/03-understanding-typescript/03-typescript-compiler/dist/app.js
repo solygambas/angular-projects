534f415e979fd33be06b681d4890029dc4da5e48
solygambas <51904909+solygambas@users.noreply.github.com>
2022-11-09 11:12:26 +0100

    add rest example

--- playground/03-understanding-typescript/03-typescript-compiler/dist/app.js
+++ playground/03-understanding-typescript/03-typescript-compiler/dist/app.js
@@ -3,3 +3,10 @@ const button = document.querySelector("button");
 button.addEventListener("click", () => {
     console.log("clicked");
 });
+const add = (...numbers) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+};
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
--- playground/03-understanding-typescript/03-typescript-compiler/src/app.ts
+++ playground/03-understanding-typescript/03-typescript-compiler/src/app.ts
@@ -3,3 +3,13 @@ const button = document.querySelector("button")!;
 button.addEventListener("click", () => {
   console.log("clicked");
 });
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addedNumbers = add(5, 10, 2, 3.7);
+
+console.log(addedNumbers);
